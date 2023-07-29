eccf00533565d4ef74cd74fc8c53e642b3c05bdd
juneyaooo <juneyaooo@gmail.com>
2023-07-29 23:54:58 +0800

    feat：添加图片上传接口与功能 1.添加图片上传接口与功能 2.修改产品表里多个图片存储的字段设置（还差逻辑没改）

--- cocreate_platform/urls.py
+++ cocreate_platform/urls.py
@@ -50,6 +50,9 @@ urlpatterns = [
     # http://127.0.0.1:8000/api/feedback/xxx
     path('api/feedback/', include('feedback.urls'), name='feedback'),
 
+    # http://127.0.0.1:8000/api/feedback/xxx
+    path('api/function/', include('function.urls'), name='function'),
+
     # http://127.0.0.1:8000/swagger/   swagger API 文档
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # http://127.0.0.1:8000/redoc/  使用Redoc UI来显示API文档
