32100c7c8266406a31f53af703facbbfd89decd6
Taylor <37405850+2951121599@users.noreply.github.com>
2023-08-10 22:08:38 +0800

    fix：修改了反馈表的字段

--- feedback/models.py
+++ feedback/models.py
@@ -19,14 +19,12 @@ class Feedback(BaseModel):
     id = UUIDField(primary_key=True, default=uuid.uuid4, verbose_name='反馈ID')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='关联用户')
     FEEDBACK_CHOICES = (
-        ('加入我们', '加入我们'),
         ('意见建议', '意见建议'),
+        ('加入我们', '加入我们'),
     )
-    feedback_type = models.CharField(max_length=50, choices=FEEDBACK_CHOICES, verbose_name='反馈类型', default="主动创建")
-    feedback_email = models.CharField(max_length=50, blank=True, verbose_name='反馈邮箱')
+    feedback_email = models.CharField(max_length=50, blank=True, verbose_name='联系方式')
+    feedback_type = models.CharField(max_length=50, choices=FEEDBACK_CHOICES, verbose_name='反馈类型', default="意见建议")
     feedback_content = models.TextField(blank=False, verbose_name='反馈内容')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
-    is_deleted = models.BooleanField(default=False, verbose_name='是否删除')
 
     def __str__(self):
         return f"反馈ID: {self.id}"
