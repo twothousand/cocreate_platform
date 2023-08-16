a03293bb362b30215910347de50b6f4bcdf50771
juneyaooo <juneyaooo@gmail.com>
2023-08-17 00:22:20 +0800

    fix:产品明细接口返回数据，加上版本号和图片的ID

--- product/views.py
+++ product/views.py
@@ -270,6 +270,16 @@ class ProductViewSet(my_mixins.LoggerMixin, my_mixins.CreatRetrieveUpdateModelVi
 
             # Check if a product with the given project_id exists
             product_instance = get_object_or_404(Product, id=product_id)
+            # # Query the ProductVersion table to get the latest version number for the given product_id
+            # latest_version = Version.objects.filter(product_id=product_id).aggregate(Max('created_at'))[
+            #     'created_at__max']
+            # print(' latest_version', latest_version)
+            # latest_version_instance = Version.objects.filter(product_id=product_id, created_at=latest_version).first()
+            #
+            # if latest_version_instance:
+            #     latest_version_number = latest_version_instance.version_number
+            # else:
+            #     latest_version_number = None
 
             # Serialize and return the product data
             product_serializer = ProductSerializer(product_instance)
@@ -300,7 +310,9 @@ class ProductViewSet(my_mixins.LoggerMixin, my_mixins.CreatRetrieveUpdateModelVi
             if not project_id:
                 response_data = {
                     'message': '项目ID未提供',
-                    'data': None,
+                    'data': {
+                        'product_id': None,
+                    },
                 }
                 return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
@@ -309,7 +321,9 @@ class ProductViewSet(my_mixins.LoggerMixin, my_mixins.CreatRetrieveUpdateModelVi
             except Project.DoesNotExist:
                 response_data = {
                     'message': '未找到符合条件的项目ID',
-                    'data': None,
+                    'data': {
+                        'product_id': None,
+                    },
                 }
                 return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
@@ -334,7 +348,8 @@ class ProductViewSet(my_mixins.LoggerMixin, my_mixins.CreatRetrieveUpdateModelVi
         except Exception as e:
             response_data = {
                 'message': '查询产品ID失败',
-                'data': {'errors': str(e)},
+                'data': {'product_id': None,
+                         'errors': str(e)},
             }
             return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
