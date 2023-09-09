4bba8c25dfd9007d98813785e07ed6ade7c23d16
LiuWH <65588374+Weihong-Liu@users.noreply.github.com>
2023-09-09 16:53:53 +0800

    fix：修改dockerfile和启动服务shell脚本

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,25 @@
 FROM python:3.9.17
 LABEL authors="puppet"
 
-ENTRYPOINT ["top", "-b"]
+# 1.安装系统插件
+# COPY ./sources.list /etc/apt/
+RUN apt-get update && apt-get install -y vim
+
+# 2.升级pip
+RUN pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+RUN pip install --upgrade pip
+
+# 容器内创建 myproject 文件夹
+ENV APP_HOME=/var/www/admin/cocreate_platform
+RUN mkdir -p $APP_HOME
+WORKDIR $APP_HOME
+
+# 将当前目录加入到工作目录中（. 表示当前目录）
+ADD . $APP_HOME
+RUN pip install -r /apps/requirements.txt
+RUN pip install gunicorn
+
+# 给start.sh可执行权限
+RUN chmod +x ./start_server.sh
+
+ENTRYPOINT /bin/bash ./start_server.sh
--- start_server.sh
+++ start_server.sh
@@ -5,41 +5,41 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 
-# 存储本地更改
-git stash
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Local changes stashed successfully.${NO_COLOR}"
-else
-    echo -e "${RED}Failed to stash local changes. Aborting.${NO_COLOR}"
-    exit 1
-fi
-
-# 拉取最新的代码
-git checkout dev
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Switched to 'dev' branch successfully.${NO_COLOR}"
-else
-    echo -e "${RED}Failed to switch to 'dev' branch. Aborting.${NO_COLOR}"
-    exit 1
-fi
-
-git pull origin dev
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Pulled latest changes from 'dev' branch successfully.${NO_COLOR}"
-else
-    echo -e "${RED}Failed to pull latest changes from 'dev' branch. Aborting.${NO_COLOR}"
-    exit 1
-fi
-
-# 应用之前存储的更改
-git stash pop
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Local changes applied successfully.${NO_COLOR}"
-else
-    echo -e "${RED}Failed to apply local changes. Aborting.${NO_COLOR}"
-    exit 1
-fi
-
+## 存储本地更改
+#git stash
+#if [ $? -eq 0 ]; then
+#    echo -e "${GREEN}Local changes stashed successfully.${NO_COLOR}"
+#else
+#    echo -e "${RED}Failed to stash local changes. Aborting.${NO_COLOR}"
+#    exit 1
+#fi
+#
+## 拉取最新的代码
+#git checkout dev
+#if [ $? -eq 0 ]; then
+#    echo -e "${GREEN}Switched to 'dev' branch successfully.${NO_COLOR}"
+#else
+#    echo -e "${RED}Failed to switch to 'dev' branch. Aborting.${NO_COLOR}"
+#    exit 1
+#fi
+#
+#git pull origin dev
+#if [ $? -eq 0 ]; then
+#    echo -e "${GREEN}Pulled latest changes from 'dev' branch successfully.${NO_COLOR}"
+#else
+#    echo -e "${RED}Failed to pull latest changes from 'dev' branch. Aborting.${NO_COLOR}"
+#    exit 1
+#fi
+#
+## 应用之前存储的更改
+#git stash pop
+#if [ $? -eq 0 ]; then
+#    echo -e "${GREEN}Local changes applied successfully.${NO_COLOR}"
+#else
+#    echo -e "${RED}Failed to apply local changes. Aborting.${NO_COLOR}"
+#    exit 1
+#fi
+#
 # # 激活conda环境
 # source activate cocreate_platform
 # if [ $? -eq 0 ]; then
@@ -50,7 +50,7 @@ fi
 # fi
 
 # 生成静态文件
-/root/miniconda3/envs/cocreate_platform/bin/python manage.py collectstatic
+python manage.py collectstatic --noinput
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Django project started successfully.${NO_COLOR}"
 else
@@ -58,12 +58,18 @@ else
     exit 1
 fi
 
+python manage.py makemigrations && python manage.py migrate
+
 # 用gunicorn启动django项目
-/root/miniconda3/envs/cocreate_platform/bin/gunicorn -c gunicorn_config.py cocreate_platform.wsgi:application -D
+gunicorn -c gunicorn_config.py cocreate_platform.wsgi:application -D
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Django project started successfully.${NO_COLOR}"
 else
     echo -e "${RED}Failed to start Django project. Aborting.${NO_COLOR}"
     exit 1
 fi
+
+tail -f /dev/null
+
+exec "$@"
 
